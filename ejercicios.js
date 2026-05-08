f860ecd4548e2a2d9dfe0adfad6126ed5e00ca73
Karina Muñoz <karina.munoz.escobar@gmail.com>
2026-05-08 12:05:00 -0400

    Readme

---
+++ ejercicios.js
@@ -0,0 +1,79 @@
+console.log("Ejercicio 1");
+let nombre = "Karina";
+let apellido = "Muñoz";
+
+console.log(nombre, apellido);
+
+//Crear variable con un número
+console.log("Ejercicio 2");
+let Numero = 10;
+//súmale 5 y multiplica x3 e imprimir resutado
+Numero = (Numero + 5) * 3;
+console.log(Numero);
+
+//Crea una variable con tu comida favorita e imprímela en consola.
+console.log("Ejercicio 3");
+let comidaFavorita = "Pizza";
+console.log("Mi comida favorita es" , comidaFavorita);
+
+//Crea dos variables a=10 y b=3
+console.log("ejercicio 4");
+console.log("DatosNumber");
+let a = 10;
+let b = 3;
+//Suma
+console.log(a+b);
+//Resta
+console.log(a-b);
+//multiplicación
+console.log(a*b);
+//Módulo
+console.log(a%b);
+
+//Cambio de datos a String
+console.log("DatosString");
+let x = "10";
+let y = "3";
+//Suma
+console.log(x+y);
+//Resta
+console.log(x-y);
+//multiplicación
+console.log(x*y);
+//Módulo
+console.log(x%y);
+
+console.log("Ejercicio 5");
+//variable edad, mayor a 10 y menor a 18
+let edad = 15;
+console.log(edad > 10);
+console.log(edad < 18);
+
+console.log("Ejercicio 6");
+let variableEdad = 8;
+console.log(variableEdad > 5);
+console.log(variableEdad < 10);
+
+console.log("Ejercicio 7");
+let nota = 6;
+console.log(nota==7);
+
+console.log("Ejercicio 8");
+let temperatura = 25;
+console.log(temperatura>=30);
+
+console.log("Ejercicio 9");
+let numero1 = 2
+let numero2 = 3
+console.log(numero1==numero2);
+console.log(numero1>numero2||numero1<numero2);
+
+console.log("Ejercicio 10");
+let edad1 = 17;
+let tienePermiso=true;
+console.log("Puede conducir", edad1 >= 18 || tienePermiso);
+
+console.log("Ejercicio 11");
+let usuario = "admin";
+let password = "1234";
+console.log("usuario es", usuario, "y", "password es", password);
